bdbc36d7b36eba11cb3a96f7f17cd98bc0fa8f08
Hammadh Arquil <hammadhy11@gmail.com>
2022-03-27 17:34:25 +0530

    Created Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="home">
+        <div class="nav-bar">
+          <div class="logo">
+            <img
+              class="logo"
+              alt="Elite's Logo"
+              src={process.env.PUBLIC_URL + "/images/elites-logo.png"}
+            />
+          </div>
+          
+          <div class="nav-items">
+            <p class="nav-item">HOME</p>
+            <p class="nav-item">ABOUT ME</p>
+            <p class="nav-item">WORK</p>
+            <p class="nav-item">CONTACT</p>
+          </div>
+      
+        </div>
+
+        <div className="detail-header"></div>
+        <div className="fields"></div>
+      </div>
+      <div className="featured"></div>
+      <div className="connect"></div>
+      <div className="footer"></div>
     </div>
   );
 }
